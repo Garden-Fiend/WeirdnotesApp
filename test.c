dbfca9983e44f58c8776b9d95e1f093cdc3f1e22
Garden Fiend <rainethegardenfiend@gmail.com>
2026-06-11 22:23:55 +0800

    first version no name yet

---
+++ test.c
@@ -0,0 +1,41 @@
+//version 1
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    int buffer = 1000;
+    int i = 0;
+    int input;
+    char textContent[buffer];
+    char selection[5];
+
+    while ((input = getchar()) != EOF)
+    {
+
+        textContent[i] = input;
+
+        if (textContent[i] == '/')
+        {
+            printf("/fin -> end writing \n /con -> proceed to write\n");
+            scanf("%s", &selection);
+
+            if (strcmp(selection,"/fin") == 0)
+            {
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        i++;
+    }
+    printf("-------------------------------------------------");
+
+    printf("\t\n%s", textContent);
+
+    return 0;
+}
